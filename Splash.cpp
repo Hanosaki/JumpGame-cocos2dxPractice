3b4eef490d5e3b675d915407f8bb1be8a922c119
Hanosaki <suikyouinga@gmail.com>
2019-04-02 21:04:44 +0900

    エラーを検出したら、処理を止めるように仕様変更

--- Splash.cpp
+++ Splash.cpp
@@ -24,56 +24,60 @@ bool Splash::init()
 	if (!Layer::init())
 		return false;
 
-	auto visibleSize = Director::getInstance()->getVisibleSize();
-	auto origin = Director::getInstance()->getVisibleOrigin();
+	try{
+		auto visibleSize = Director::getInstance()->getVisibleSize();
+		auto origin = Director::getInstance()->getVisibleOrigin();
 
-	auto gf = new GenericFunc();
-	gf->crashBox("sample", this);
-
-	auto reader = new FileRead();
-	//auto folder = reader->sReadFile("csv/folder",this);
-	
-	CCLOG("I'm OK");
-
-
-	//char* seName;
+		auto reader = new FileRead();
+		auto folder = reader->sReadFile("csv/folder");
+		if (folder.size() <= 0)
+			throw "ファイルが正常に読み込めませんでした。";
+		
+		//char* seName;
 
 #if(CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)//Windows用SEロード
-	
-	//seName = Converter::replaceDATtoMP3();
-	
+
+		//seName = Converter::replaceDATtoMP3();
+
 #elif(CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)//Android用SEロード処理
 
-	seName = Converter::replaceString2Char(F_SE + LOGO_SE + TYPE_MP3);
+		seName = Converter::replaceString2Char(F_SE + LOGO_SE + TYPE_MP3);
 
 #endif
-	
-	AudioEngine::preload(F_SE + LOGO_SE);
+
+		AudioEngine::preload(F_SE + LOGO_SE);
 
 #pragma region ロゴ表示
-	auto logo = Label::createWithTTF(LOGO, F_FONTS + JPN_FONTS, 64);
-	logo->setTextColor(Color4B::WHITE);
-	logo->setPosition(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2);
-	logo->setOpacity(0);
-	this->addChild(logo);
+		auto logo = Label::createWithTTF(LOGO, F_FONTS + JPN_FONTS, 64);
+		logo->setTextColor(Color4B::WHITE);
+		logo->setPosition(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2);
+		logo->setOpacity(0);
+		this->addChild(logo);
 #pragma endregion
 
 #pragma region ロゴアニメーション
 
-	auto logoAction = Sequence::create(FadeIn::create(1.0f), DelayTime::create(2.0f), FadeOut::create(1.0f), NULL);
-	logo->runAction(logoAction);
+		auto logoAction = Sequence::create(FadeIn::create(1.0f), DelayTime::create(2.0f), FadeOut::create(1.0f), NULL);
+		logo->runAction(logoAction);
 
 #pragma endregion
 
-	//auto seId = AudioEngine::play2d(seName,false,0.3f,nullptr);
+		//auto seId = AudioEngine::play2d(seName,false,0.3f,nullptr);
 
-	this->scheduleOnce(schedule_selector(Splash::callLoadScene), 5.0f);
+		this->scheduleOnce(schedule_selector(Splash::callLoadScene), 5.0f);
 
 #pragma region クリック処理の初期設定
-	auto listner = EventListenerTouchOneByOne::create();
-	listner->onTouchBegan = CC_CALLBACK_2(Splash::onTouchBegan, this);
-	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listner, this);
+		//auto listner = EventListenerTouchOneByOne::create();
+		//listner->onTouchBegan = CC_CALLBACK_2(Splash::onTouchBegan, this);
+		//Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listner, this);
 #pragma endregion
+
+	}
+	catch (char* message)
+	{
+		auto gf = new GenericFunc();
+		gf->crashBox(message, this);
+	}
 
 	return true;
 
