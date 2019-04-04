50db434537e483aceb689ddaa6df3c03ce469eae
Hanosaki <suikyouinga@gmail.com>
2019-04-04 14:34:45 +0900

    処理の細分化作業開始、ラベル表示を別クラスに移行、音再生も別クラスでまとめて処理する方向で検討中

--- Splash.cpp
+++ Splash.cpp
@@ -5,8 +5,8 @@
 #include "CharaResouse.h"
 #include "Converter.h"
 #include "FileReadClass.h"
-
-#include "GenericFunction.h"
+#include "MenuWindow.h"
+#include "createLabel.h"
 
 USING_NS_CC;
 using namespace experimental;
@@ -27,54 +27,37 @@ bool Splash::init()
 	try{
 		auto visibleSize = Director::getInstance()->getVisibleSize();
 		auto origin = Director::getInstance()->getVisibleOrigin();
+		auto winCenter = Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2);
 
-		auto reader = new FileRead();
-		auto folder = reader->sReadFile("csv/folder");
-		
-		//char* seName;
-
-#if(CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)//Windows用SEロード
-
-		//seName = Converter::replaceDATtoMP3();
-
-#elif(CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)//Android用SEロード処理
-
-		seName = Converter::replaceString2Char(F_SE + LOGO_SE + TYPE_MP3);
-
-#endif
-
-		AudioEngine::preload(F_SE + LOGO_SE);
-
-#pragma region ロゴ表示
-		auto logo = Label::createWithTTF(LOGO, F_FONTS + JPN_FONTS, 64);
-		logo->setTextColor(Color4B::WHITE);
-		logo->setPosition(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2);
-		logo->setOpacity(0);
-		this->addChild(logo);
-#pragma endregion
+		//ロゴ表示(ラベル生成用クラスを作成する)
+		CreateLabel::setLabel(LOGO,F_FONTS+JPN_FONTS,64,Color4B::WHITE,winCenter,1,this);
 
 #pragma region ロゴアニメーション
 
+		auto logo = (Label*)this->getChildByTag(1);
 		auto logoAction = Sequence::create(FadeIn::create(1.0f), DelayTime::create(2.0f), FadeOut::create(1.0f), NULL);
 		logo->runAction(logoAction);
 
 #pragma endregion
 
-		//auto seId = AudioEngine::play2d(seName,false,0.3f,nullptr);
+		auto seNames = FileRead::sReadFile(SE_POINT_CSV);
+		auto seName = seNames["LOGO"];
+		AudioEngine::preload(seName);
+		AudioEngine::play2d(seName,false,0.3f,nullptr);
 
 		this->scheduleOnce(schedule_selector(Splash::callLoadScene), 5.0f);
 
 #pragma region クリック処理の初期設定
-		//auto listner = EventListenerTouchOneByOne::create();
-		//listner->onTouchBegan = CC_CALLBACK_2(Splash::onTouchBegan, this);
-		//Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listner, this);
+		auto listner = EventListenerTouchOneByOne::create();
+		listner->onTouchBegan = CC_CALLBACK_2(Splash::onTouchBegan, this);
+		Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listner, this);
 #pragma endregion
 
 	}
 	catch (char* message)
 	{
-		auto gf = new GenericFunc();
-		gf->crashBox(message, this);
+		auto mw = new MenuWindow();
+		mw->crashBox(message, this);
 	}
 
 	return true;
