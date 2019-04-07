c6b0a5182ab2cff3500f079643d62a29e40675dd
Hanosaki <suikyouinga@gmail.com>
2019-04-07 17:18:36 +0900

    アニメーション画像のキャッシュを作成するクラスの作成

---
+++ AnimationLoad.h
@@ -0,0 +1,18 @@
+#ifndef __ANIMATION_LOAD_H__
+#define __ANIMATION_LOAD_H__
+
+#include "cocos2d.h"
+
+class AnimationLoad
+{
+public:
+
+	static void load();
+
+private:
+
+	void animeLoad();
+
+};
+
+#endif
